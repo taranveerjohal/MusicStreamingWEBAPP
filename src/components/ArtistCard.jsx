e0bd828663b587daaab16cf2e9c1034a35bf7a12
taranveerjohal <32153442+taranveerjohal@users.noreply.github.com>
2023-02-02 21:41:45 +1030

    fixed top artist

--- src/components/ArtistCard.jsx
+++ src/components/ArtistCard.jsx
@@ -1,15 +1,11 @@
 import { useNavigate } from "react-router-dom";
-import { useEffect, useRef } from 'react';
 
 const ArtistCard = ({track}) => {
   const navigate = useNavigate()
-  useEffect(() => {
-    divref.current.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
-  });
-  const divref = useRef(null);
+
   return(
       track.images?.coverart && track?.hub.actions &&
-      <div ref={divref} className="flex flex-col w-[250px] p-4 bg-white/5
+      <div className="flex flex-col w-[250px] p-4 bg-white/5
        bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer" onClick={() => navigate(`/artists/${track?.artists[0].adamid}`)}>
         <img src={track?.images?.coverart} alt={track?.title} className="w-full h-56 rounded-lg" />
         <div className="flex flex-col justify-between mt-2">
